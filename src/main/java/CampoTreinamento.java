aae5d1d5f4b87f08c5b89513093f75ab04be5b07
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-01-22 11:11:38 -0300

    Configurando as pastas para realizar os testes no Campo de Treinamento

--- src/main/java/CampoTreinamento.java
+++ src/main/java/CampoTreinamento.java
@@ -3,10 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CampoTreinamento {
-	@Test
-	public void Teste() {
+	public void inicializar() {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("file:///" + System.getProperty("user.dir")+ "src/main/resources/componentes.html");
+		driver.get("file:///" + System.getProperty("user.dir")+ "/src/main/resources/componentes.html");
 	}
 }
