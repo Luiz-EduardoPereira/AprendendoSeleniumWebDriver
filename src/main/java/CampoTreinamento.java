45aeff49fef8ffb00664765a905e0a3f4ee7db81
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-01-22 08:48:22 -0300

    Preparando ambiente para a classe CampoTreinamento

--- src/main/java/CampoTreinamento.java
+++ src/main/java/CampoTreinamento.java
@@ -1,4 +1,12 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CampoTreinamento {
-	
+	@Test
+	public void Teste() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir")+ "src/main/resources/componentes.html");
+	}
 }
