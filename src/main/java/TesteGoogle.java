db0eb295335c8e44e5d0964a3b553a8a1ab68909
Pantheon <luizmaki7@hotmail.com>
2021-01-22 07:35:41 -0300

    Mandando atualizações

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,33 +1,47 @@
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteGoogle {
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver;
+	public void pausa() {
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
 	public void inicializar() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 	@Test
-	public void abrirGoogleChrome() {
+	public void abrirGuiaGoogle() {
 		inicializar();
-		driver.get("https://www.google.com.br");
+		pausa();
+		driver.get("https://www.google.com.br/");
 	}
 	@Test
-	public void realizarPesquisa(){
-		
+	public void realizarPesquisaGoogle(){
+		inicializar();
+		abrirGuiaGoogle();
+		pausa();
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Java");
+		driver.findElement(By.xpath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']")).click();
 	}
 	@Test
 	public void validarTituloGoogle() {
 		inicializar();
-		abrirGoogleChrome();
+		abrirGuiaGoogle();
+		pausa();
 		Assert.assertEquals("Google", driver.getTitle());
 	}
 	@Test
 	public void validarUrlGoogle() {
 		inicializar();
-		abrirGoogleChrome();
+		abrirGuiaGoogle();
+		pausa();
 		Assert.assertEquals("https://www.google.com.br/", driver.getCurrentUrl());
 	}
 }
