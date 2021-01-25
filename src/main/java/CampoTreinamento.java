e9837750615adf6c85ff4faf0f6245fcf410813c
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-01-24 22:06:05 -0300

    Enviando atualizações na classe CampoTreinamento sobre os elementos básicos.

--- src/main/java/CampoTreinamento.java
+++ src/main/java/CampoTreinamento.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CampoTreinamento {
-	WebDriver driver;
+	private static WebDriver driver;
 	public void inicializar() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
@@ -20,6 +20,16 @@ public class CampoTreinamento {
 		Assert.assertEquals("Nuyhe", driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
 	}
 	@Test
+	public void limparTextField() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Wies");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Nuyhe");
+		driver.findElement(By.id("elementosForm:nome")).clear();
+		driver.findElement(By.id("elementosForm:sobrenome")).clear();
+		Assert.assertEquals("", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		Assert.assertEquals("", driver.findElement(By.id("elementosForm:sobrenome")).getAttribute("value"));
+	}
+	@Test
 	public void usarTextArea() {
 		inicializar();
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ");
@@ -30,5 +40,52 @@ public class CampoTreinamento {
 		inicializar();
 		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Agile\nQA\nPO\nDEV");
 		Assert.assertEquals("Agile\nQA\nPO\nDEV", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+	}
+	@Test
+	public void limparTextArea() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Agile\nQA\nPO\nDEV");
+		driver.findElement(By.id("elementosForm:sugestoes")).clear();
+		Assert.assertEquals("", driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value"));
+	}
+	@Test
+	public void usarRadioButton() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+	}
+	@Test
+	public void usarRadioButtonAlternando() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:sexo:1")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:1")).isSelected());
+	}
+	@Test
+	public void usarCheckBox() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+	}
+	@Test
+	public void usarCheckBoxMultiplo() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:1")).click();		
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:1")).isSelected());	
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:2")).isSelected());
+	}
+	@Test
+	public void limparCheckBox() {
+		inicializar();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		Assert.assertFalse(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+	}
+	@Test
+	public void usarComboBox() {
+		
 	}
 }
