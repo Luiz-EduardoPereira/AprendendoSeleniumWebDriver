7c6236f583d4b57dcdfb5cf1e0ea8016f9ba27d0
Luiz Eduardo <luiz.eduardo@boavista.local>
2021-02-04 11:05:08 -0300

    Criada metódo para realizar cadastro com multiplas opções e um outro para validar o cadastro múltiplo.

--- src/main/java/Cadastro.java
+++ src/main/java/Cadastro.java
@@ -40,5 +40,35 @@ public class Cadastro{
 		Assert.assertEquals("Esportes: Futebol", driver.findElement(By.id("descEsportes")).getText());
 		Assert.assertEquals("Sugestoes: Devo aprender a fazer automatização com o Selenium WebDriver.", driver.findElement(By.id("descSugestoes")).getText());
 	}
+	@Test
+	public void realizarCadastroComOpcoesDiversas() {
+		inicialiazar();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Luiz");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Eduardo");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		WebElement elementoComboUnico = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select comboUnico = new Select(elementoComboUnico);
+		comboUnico.selectByVisibleText("Superior");
+		WebElement elementoComboMultiplo = driver.findElement(By.id("elementosForm:esportes"));
+		Select comboMultiplo = new Select(elementoComboMultiplo);
+		comboMultiplo.selectByVisibleText("Futebol");
+		comboMultiplo.selectByVisibleText("Natacao");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Devo aprender a fazer automatização com o Selenium WebDriver.");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+		validarCadastroMultiplo();
+	}
+	public void validarCadastroMultiplo() {
+		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
+		Assert.assertEquals("Nome: Luiz", driver.findElement(By.id("descNome")).getText());
+		Assert.assertEquals("Sobrenome: Eduardo", driver.findElement(By.id("descSobrenome")).getText());
+		Assert.assertEquals("Sexo: Masculino", driver.findElement(By.id("descSexo")).getText());
+		Assert.assertEquals("Comida: Carne Pizza", driver.findElement(By.id("descComida")).getText());
+		Assert.assertEquals("Escolaridade: superior", driver.findElement(By.id("descEscolaridade")).getText());
+		Assert.assertEquals("Esportes: Natacao Futebol", driver.findElement(By.id("descEsportes")).getText());
+		Assert.assertEquals("Sugestoes: Devo aprender a fazer automatização com o Selenium WebDriver.", driver.findElement(By.id("descSugestoes")).getText());
+	}
+
 }
 
