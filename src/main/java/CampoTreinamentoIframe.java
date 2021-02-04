e7c1ff31939c6b4dbc748628e2586b708c6e232a
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-02-04 14:35:14 -0300

    Terminado o metódo clicarBtnDentroFrame

--- src/main/java/CampoTreinamentoIframe.java
+++ src/main/java/CampoTreinamentoIframe.java
@@ -1,4 +1,28 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CampoTreinamentoIframe {
 
+public class CampoTreinamentoIframe{
+	public static WebDriver driver;
+	public void inicializar() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir")+ "/src/main/resources/componentes.html");
+	}
+	@Test
+	public void clicarBtnDetroFrame() {
+		inicializar();
+		driver.switchTo().frame("frame1");
+		driver.findElement(By.id("frameButton")).click();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Frame OK!", alerta.getText());
+		alerta.accept();
+		driver.switchTo().defaultContent();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Frame button OK!");
+		Assert.assertEquals("Frame button OK!", driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+	}
 }
