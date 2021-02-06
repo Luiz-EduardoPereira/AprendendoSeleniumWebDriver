fa30ba90263ae9c5f24219bd6ac6194a9532b363
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-02-05 21:32:39 -0300

    Enviando atualizações na classe CampoTreinamentoRegraNegocio

--- src/main/java/CampoTreinamentoRegraNegocio.java
+++ src/main/java/CampoTreinamentoRegraNegocio.java
@@ -1,4 +1,17 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CampoTreinamentoRegraNegocio {
-
+	public static WebDriver driver;
+	public void inicialiazar() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir")+ "/src/main/resources/componentes.html");
+	}
+	@Test
+	public void testarRegraNegocio() {
+		driver.findElement(By.id("")).click();
+	}
 }
