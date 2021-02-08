b5f451681dfc2c87a4fbff80a19796f40cb6fcac
Luiz-EduardoPereira <luiz.eduardop@outlook.com>
2021-02-08 13:13:37 -0300

    Enviando atualizações na classe CampoTreinamentoRegraNegocio

--- src/main/java/CampoTreinamentoRegraNegocio.java
+++ src/main/java/CampoTreinamentoRegraNegocio.java
@@ -1,7 +1,12 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class CampoTreinamentoRegraNegocio {
 	public static WebDriver driver;
@@ -10,8 +15,57 @@ public class CampoTreinamentoRegraNegocio {
 		driver.manage().window().maximize();
 		driver.get("file:///" + System.getProperty("user.dir")+ "/src/main/resources/componentes.html");
 	}
+	public void btnCadastrar() {
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
+	}
+	public void regraNegocioNome() {
+		inicialiazar();
+		btnCadastrar();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Nome eh obrigatorio", alerta.getText());
+		alerta.accept();
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Usxowu");
+	}
+	public void regraNegocioSobrenome() {
+		btnCadastrar();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Sobrenome eh obrigatorio", alerta.getText());
+		alerta.accept();
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Laostmodea");
+	}
+	public void regraNegocioSexo() {
+		btnCadastrar();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Sexo eh obrigatorio", alerta.getText());
+		alerta.accept();
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+	}
+	public void regraNegocioComida() {
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+		btnCadastrar();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", alerta.getText());
+		alerta.accept();
+		driver.findElement(By.id("elementosForm:comidaFavorita:3")).click();
+	}
+	public void regraNegocioEsporte() {
+		WebElement opcoes = driver.findElement(By.id("elementosForm:esportes"));
+		Select combo = new Select(opcoes);
+		combo.selectByVisibleText("Natacao");
+		combo.selectByVisibleText("O que eh esporte?");
+		btnCadastrar();
+		Alert alerta = driver.switchTo().alert();
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", alerta.getText());
+		alerta.accept();
+		combo.selectByVisibleText("Natacao");
+	}
 	@Test
 	public void testarRegraNegocio() {
-		driver.findElement(By.id("")).click();
+		regraNegocioNome();
+		regraNegocioSobrenome();
+		regraNegocioSexo();
+		regraNegocioComida();
+		btnCadastrar();
 	}
 }
