605c1c7cfea06aa83c72ed37ac10e437c7cee472
Luiz Eduardo <luiz.eduardo@boavista.local>
2021-02-18 19:43:59 -0300

    Finalizada a classe que realiza testes parametrizaveis

--- src/main/java/TesteRegrasCadastro.java
+++ src/main/java/TesteRegrasCadastro.java
@@ -5,18 +5,33 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
+@RunWith(Parameterized.class)
 public class TesteRegrasCadastro {
 	private static WebDriver driver;
-	private CampoTreinamentoPage page;
-	public String sobrenome;
+	private CampoTreinamentoPage page; 
+	
+	@Parameter
 	public String nome;
+	@Parameter(value=1)
+	public String sobrenome;
+	@Parameter(value=2)
 	public String sexo;
+	@Parameter(value=3)
 	public List<String> comidas;
+	@Parameter(value=4)
 	public String[] esportes;
+	@Parameter(value=5)
 	public String msg;
+	
 	@Before
 	public void inicializar() {
 		driver = new ChromeDriver();
@@ -28,11 +43,17 @@ public class TesteRegrasCadastro {
 	public void fecharBrowser() {
 		driver.quit();
 	}
+	@Parameters
 	public static Collection<Object[]> getCollection(){
 		return Arrays.asList(new Object[][] {
-			
+			{"", "", "", Arrays.asList(), new String[]{}, "Nome eh obrigatorio"},
+			{"Luiz", "", "", Arrays.asList(), new String[]{}, "Sobrenome eh obrigatorio"},
+			{"Luiz", "Eduardo", "", Arrays.asList(), new String[]{}, "Sexo eh obrigatorio"},
+			{"Luiz", "Eduardo", "Masculino", Arrays.asList("Carne","Vegetariano"), new String[]{}, "Tem certeza que voce eh vegetariano?"},
+			{"Luiz", "Eduardo", "Masculino", Arrays.asList("Carne"), new String[]{"Karate", "O que eh esporte?"}, "Voce faz esporte ou nao?"},
 		});
 	}
+	@Test
 	public void validarRegras() {
 		page.inserirNome(nome);
 		page.inserirSobrenome(sobrenome);
@@ -46,6 +67,7 @@ public class TesteRegrasCadastro {
 		if (comidas.contains("Vegetariano")) page.setComidaVegetariano();
 		page.setEsporte(esportes);
 		page.cadastrar();
+		System.out.println(msg);
 		Assert.assertEquals(msg, page.obterTextoAlertEAceitar());
 	}
 }
