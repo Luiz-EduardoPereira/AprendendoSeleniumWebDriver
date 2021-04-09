5b57b356310970ba02e4b5a3bc9e412f25b84c04
Luiz Eduardo <luiz.eduardo@boavista.local>
2021-04-08 23:04:34 -0300

    Criada as classes para teste de sincronismo e uma para testar o ajax.

---
+++ src/main/java/TesteAjax.java
@@ -0,0 +1,34 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TesteAjax {
+	private WebDriver driver;
+	private DSL dsl;
+	
+	@Before
+	public void inicializar() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.primefaces.org/showcase/ui/ajax/basic.xhtml?jfwid=71d53");
+		dsl = new DSL(driver);
+	}
+	@After
+	public void fecharBrowser() {
+		driver.quit();
+	}
+	@Test
+	public void testarAjax() { 
+		dsl.escrever("j_idt302:name", "Teste");
+		dsl.clicar(By.id("j_idt302:j_idt306"));
+		WebDriverWait espera = new WebDriverWait(driver, 30);
+		espera.until(ExpectedConditions.textToBe(By.id("j_idt302:display"), "Teste"));
+		Assert.assertEquals("Teste", dsl.obterTexto("j_idt302:display"));
+	}
+}
